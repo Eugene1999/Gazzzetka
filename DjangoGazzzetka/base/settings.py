4d8abab84c14f244f294705d26bd7737ff3424c1
Yevhen Lehkiy <yevhen.lehkyi@completecase.com>
2025-01-17 09:24:36 +0200

    Move django secret to .env

--- DjangoGazzzetka/base/settings.py
+++ DjangoGazzzetka/base/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f$yjt#68)+q@q@t&y92#5k@o&_mq$6hph1#6-q@2*5&%f*=%9!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
