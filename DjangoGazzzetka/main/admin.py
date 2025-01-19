49e2cd0c80cee7beee466ace9b8d097be04cc813
Yevhen Lehkiy <yevhen.lehkyi@completecase.com>
2025-01-19 13:39:22 +0200

    Init migration and add tables to admin page

--- DjangoGazzzetka/main/admin.py
+++ DjangoGazzzetka/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, ThemeColor, Channel, Article, ArticleImage, ArticleRelease
+
+admin.site.register(User)
+admin.site.register(ThemeColor)
+admin.site.register(Channel)
+admin.site.register(Article)
+admin.site.register(ArticleImage)
+admin.site.register(ArticleRelease)
